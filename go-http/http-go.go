92041791d5ef981a18c4f2d59dd4a6a796a77398
Elena-Weber <crochetmiracles@gmail.com>
2023-11-29 18:43:11 -0500

    add http

---
+++ go-http/http-go.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type logWriter struct{}
+
+func main() {
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	lw := logWriter{}
+
+	// takes Writer (destination of resulting data) and Reader (source of incoming data)
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("The app wrote this many bytes:", len(bs))
+	return len(bs), nil
+}
