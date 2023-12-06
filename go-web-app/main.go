203ddcc42fe7f943bf6e34782629bb2b41f8571d
Elena-Weber <crochetmiracles@gmail.com>
2023-12-06 15:27:03 -0500

    add handlers and more pages

--- go-web-app/main.go
+++ go-web-app/main.go
@@ -5,15 +5,32 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// capital letter function name means it's accessible from outside
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "HOME")
+}
+
+ // About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 3)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("ABOUT %d", sum)) // string print with variable type int
+}
+
+ // small letter function name means it's private
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main app function
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		n, err := fmt.Fprintf(w, "Hello World") // prints on webpage
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n)) // prints in console
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	// Sprtintf = string print with variable type string
+	fmt.Println(fmt.Sprintf("Starting app on port %s", portNumber))
 
 	//ignore errors, listen to port http://localhost:8080/
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(portNumber, nil)
 }
