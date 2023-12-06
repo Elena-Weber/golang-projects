3debcd13d212ff68ae6a38ce7edbe421e914b74f
Elena-Weber <crochetmiracles@gmail.com>
2023-12-06 15:58:13 -0500

    add templates

--- go-web-app/main.go
+++ go-web-app/main.go
@@ -1,54 +1,39 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const portNumber = ":8080"
 
 // capital letter function name means it's accessible from outside
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HOME")
+	renderTemplate(w, "home.page.tmpl")
 }
 
  // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 3)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("ABOUT %d", sum)) // string print with variable type int
+	renderTemplate(w, "about.page.tmpl")
 }
 
- // small letter function name means it's private
-func addValues(x, y int) int {
-	return x + y
-}
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	// parse templates
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 
-func Divide(w http.ResponseWriter, r *http.Request) { // pointer to request
-	f, err := divideValues(100.0, 5.0)
+	// handle errors from parsed template
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprint(w, "Cannot divide by 0")
+		fmt.Println("Error parsing a template:", err)
 		return
 	}
-
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 5.0, f)) // string print with variable type float32
-}
-
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("Cannot divide by zero")
-		return 0, err
-	}
-
-	result := x / y
-	return result, nil
 }
 
 // main is the main app function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	// Sprtintf = string print with variable type string
 	fmt.Println(fmt.Sprintf("Starting app on port %s", portNumber))
