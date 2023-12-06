073934c617a982c1983665f1a921fe592c9be868
Elena-Weber <crochetmiracles@gmail.com>
2023-12-06 15:40:41 -0500

    add basic error handling

--- go-web-app/main.go
+++ go-web-app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -23,10 +24,31 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) { // pointer to request
+	f, err := divideValues(100.0, 5.0)
+	if err != nil {
+		fmt.Fprint(w, "Cannot divide by 0")
+		return
+	}
+
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 5.0, f)) // string print with variable type float32
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+
+	result := x / y
+	return result, nil
+}
+
 // main is the main app function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	// Sprtintf = string print with variable type string
 	fmt.Println(fmt.Sprintf("Starting app on port %s", portNumber))
