4ab2abcfcdd93589b2906ec1628502e375ec65f0
Elena-Weber <crochetmiracles@gmail.com>
2023-12-11 13:07:09 -0500

    add chi and middleware for routing

--- go-web-app/cmd/web/routes.go
+++ go-web-app/cmd/web/routes.go
@@ -3,17 +3,30 @@ package main
 import (
 	"go-web-app/pkg/config"
 	"net/http"
-	"github.com/bmizerany/pat"
+	// "github.com/bmizerany/pat"
 	"go-web-app/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	// default server mux
-  mux := pat.New()
+  mux := chi.NewRouter()
+
+	// add middleware to handle aoo panics
+	mux.Use(middleware.Recoverer)
 
 	// routes
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
+
+	// THIS IS PAT APPROACH
+	// // default server mux
+  // mux := pat.New()
+	// // routes
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// return mux
 }
